709f55a88ddd2d7cbefcb8792fe3bffc89e0ec55
Arik Alexandrov <arikxl@gmail.com>
2023-04-21 23:10:38 +0300

    replace

---
+++ replace.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str ='Hello world! python is the best language!'
+    word_to_replace = input('Enter a word to replace: ')
+    new_word = input('Enter the word replacement: ')
+    print(str.replace(word_to_replace, new_word))
+
+replace_word()
